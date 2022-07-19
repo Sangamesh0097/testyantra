e7588f31bcb80b2d17ffb6b120b9793794684c8e
satishss075 <109574885+satishss075@users.noreply.github.com>
2022-07-19 17:30:36 +0530

    Update TestngTest.java

--- MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/TestngTest.java
+++ MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/TestngTest.java
@@ -18,16 +18,16 @@ public class TestngTest {
 	
 	@Test
 	public void testCase2Test() {
-		System.out.println("test case 2");
+		System.out.println("test case 4");
 	}
 	
 	@Test
 	public void testCase3Test() {
-		System.out.println("test case 3");
+		System.out.println("test case 5");
 	}
 	
 	@Test
 	public void testCase4Test() {
-		System.out.println("test case 4");
+		System.out.println("test case 6");
 	}
 }
